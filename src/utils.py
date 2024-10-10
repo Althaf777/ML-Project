7c7dc389fcbd6786cc9e9f69d3efa02a24cd4c06
Althaf777 <althafpalemad777@gmail.com>
2024-10-10 19:45:13 +0530

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -34,7 +34,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-            
+
             y_train_pred=model.predict(X_train)
 
             y_test_pred=model.predict(X_test)
@@ -46,5 +46,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]]=test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e,sys)
